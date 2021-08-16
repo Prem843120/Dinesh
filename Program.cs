24d560e3488b1191cc0b72567ca604ec40ab9af0
Prem843120 <88933059+Prem843120@users.noreply.github.com>
2021-08-16 21:44:06 +0530

    test
    test

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TestA
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string std = null;
+
+
+            try
+            {
+                //PrintStudentName(std);
+
+                if (std == null)
+                    throw new NullReferenceException("Student object is null.");
+
+                Console.WriteLine("Prem");
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ;
+                Console.WriteLine("xx");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+              
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void PrintStudentName(string std)
+        {
+            if (std == null)
+                throw new NullReferenceException("Student object is null.");
+
+            Console.WriteLine("Prem");
+        }
+
+    }
+}
